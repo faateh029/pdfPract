03e57132fc3d808a86149a46d7b58034009e6d8b
faateh029 <buttfatih691@gmail.com>
2025-09-13 16:18:54 +0500

    complete pdfGenerator func

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,7 @@
 import path from 'path';
 import pdf from 'pdf-creator-node';
+import {options} from '../helpers/options.js';
+import {data} from '../helpers/data.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import fs from 'fs';
@@ -13,4 +15,43 @@ export const homeView = (req , res)=>{
 export const generatePdf = (req , res , next)=>{
     const html = fs.readFileSync(__dirname , "../views/templates.html" , "utf-8");
     const filename = Math.random()+'_doc' + '.pdf';
+    let array=[];
+    data.forEach(p=>{
+        const prod = {
+            name:p.name,
+            description:p.description,
+            quantity:p.quantity,
+            unit:p.unit,
+            imgurl:p.imgurl
+        }
+        array.push(prod);
+    })
+    let subTotal = 0 ;
+    array.forEach(i=>{
+       subTotal+=i.total;
+    })
+    const tax = (subTotal*20)/100;
+    const grandTotal = subTotal-tax;
+    const obj = {
+        prodList:array,
+        subTotal:subTotal,
+        tax,
+        gTotal:grandTotal
+    }
+    const document = {
+        html:html,
+        data:{
+            products:obj
+        },
+        path:'./docs'+filename
+    }
+    pdf.create(document , options).then(res=>{
+          console.log(res);
+    }).catch(error=>{
+       console.log(error);
+    });
+    const filePath = 'http://localhost:3001/docs/'+filename;
+    res.render('download' , {
+        path:filePath
+    })
 }
