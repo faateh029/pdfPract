1401006b3f2d05c079c864f80b63a00d750b2818
faateh029 <buttfatih691@gmail.com>
2025-09-13 15:51:20 +0500

    setup data.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,16 @@
 import path from 'path';
 import pdf from 'pdf-creator-node';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import fs from 'fs';
-
+// Create __filename and __dirname equivalents
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 export const homeView = (req , res)=>{
     res.render('home');
+}
+
+export const generatePdf = (req , res , next)=>{
+    const html = fs.readFileSync(__dirname , "../views/templates.html" , "utf-8");
+    const filename = Math.random()+'_doc' + '.pdf';
 }
