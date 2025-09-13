ed3f4b7e7ea20a6b6df5219fda9d3f98d720d3fe
faateh029 <buttfatih691@gmail.com>
2025-09-13 15:23:18 +0500

    setup homepage

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ import expressLayouts from 'express-ejs-layouts';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
+import {homeRouter} from './routes/home.routes.js';
 
 // Create __filename and __dirname equivalents
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = new express();
+app.set('view engine' , 'ejs');
 app.use(expressLayouts);
-app.set('view-engine' , 'ejs');
+
 app.use(express.static(path.join(__dirname , 'public'))) ;
-app.listen(3000,()=>{
-   console.log("server running on port 3000");
+app.use('/', homeRouter);
+
+app.listen(3001,()=>{
+   console.log("server running on port 3001");
 })
--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
 import {Router} from 'express';
 export const homeRouter = new Router();
-import { homeView } from '../controllers/homeController';
+import { homeView } from '../controllers/homeController.js';
 homeRouter.get('/' , homeView);
