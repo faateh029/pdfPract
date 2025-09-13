df30c9357f5cd6672abdb6d52a99dab09a8bf684
faateh029 <buttfatih691@gmail.com>
2025-09-13 14:45:43 +0500

    setup server

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Create __filename and __dirname equivalents
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = new express();
+app.use(expressLayouts);
+app.set('view-engine' , 'ejs');
+app.use(express.static(path.join(__dirname , 'public'))) ;
+app.listen(3000,()=>{
+   console.log("server running on port 3000");
+})
